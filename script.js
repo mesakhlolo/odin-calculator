c0826d75783b437f057091e7ed13e68960cb058e
mesakhlolo <mesakhlolo12@gmail.com>
2023-07-18 18:37:45 +0700

    dom select in javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+
+const clear = document.querySelector("#clear-btn");
+const equal = document.querySelector(".equal");
+const decimal = document.querySelector(".decimal");
+
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+const previousScreen = document.querySelector(".previous");
+const currentScreen = document.querySelector(".current");
